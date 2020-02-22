5350bedf910b00617ddcfe94418a14da5904bebb
ip11 <iprash011@gmail.com>
2020-02-22 15:09:29 +0530

    Clear Users from State

--- src/App.js
+++ src/App.js
@@ -15,24 +15,34 @@ class App extends React.Component {
 
 	async componentDidMount() {
 		this.setState({ loading: true });
-		const res = await axios.get(`https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_SECRET}`);
+		const res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env
+				.GITHUB_SECRET}`
+		);
 
 		this.setState({ users: res.data, loading: false });
 	}
 
-	searchUsers= async (text)=>{
+	searchUsers = async (text) => {
 		this.setState({ loading: true });
 
-		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_SECRET}`);
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env
+				.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_SECRET}`
+		);
 
 		this.setState({ users: res.data.items, loading: false });
-	}
+	};
+
+	clearUsers = () => {
+		this.setState({users : [], loading : false})
+	};
 	render() {
 		return (
 			<div className="App">
 				<NavBar />
 				<div className="container">
-					<Search searchUsers={this.searchUsers}/>
+					<Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
 					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import PropTypes from "prop-types";
-
+import PropTypes from 'prop-types';
 
 class Search extends Component {
 	state = {
 		text: ''
-    };
-    
-    static propTypes = {
-        searchUsers : PropTypes.func.isRequired
-    }
+	};
+
+	static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers : PropTypes.func.isRequired
+	};
 	onChange = (e) => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
@@ -32,6 +32,8 @@ class Search extends Component {
 					/>
 					<input type="submit" value="Search" className="btn btn-dark btn-block" />
 				</form>
+                <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
+
 			</div>
 		);
 	}
