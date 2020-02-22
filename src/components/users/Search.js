084faf0ba0bec0689ec79879ade40a666d9db614
ip11 <iprash011@gmail.com>
2020-02-22 14:44:54 +0530

    Form onChange

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -5,12 +5,15 @@ class Search extends Component {
     state = {
         text : ""
     }
+    onChange=(e)=>{
+        this.setState({text : e.target.value})
+    }
 
 	render() {
 		return (
 			<div>
 				<form className="form">
-					<input type="text" name="text" placeholder="Search Users .." value={this.state.text}  />
+					<input type="text" name="text" placeholder="Search Users .." value={this.state.text} onChange={this.onChange} />
 					<input type="submit" value="Search" className="btn btn-dark btn-block" />
 				</form>
 			</div>
