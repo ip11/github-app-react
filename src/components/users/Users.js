842d44e2c55de69f3f037a9138c47efe577414e6
ip11 <iprash011@gmail.com>
2020-02-22 13:40:05 +0530

    Add UsersStyle grid

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -26,7 +26,7 @@ import UserItem from "./UserItem";
 
     render() {
         return (
-            <div>
+            <div style={userStyle}>
                 {this.state.users.map(user => (
                     <UserItem key={user.id} user={user}/>
                         
@@ -35,6 +35,12 @@ import UserItem from "./UserItem";
             </div>
         )
     }
+}
+
+const userStyle = {
+    display : 'grid',
+    gridTemplateColumns : 'repeat(3,1fr)',
+    gridGap : '1rem'
 }
 
 export default Users
