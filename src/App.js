5d22ef8d3ddc406b376e274ba479324d53731f8d
ip11 <iprash011@gmail.com>
2020-02-22 13:05:07 +0530

    Normal State from GitHub API

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 //Import Components
 import NavBar from './components/layouts/NavBar';
-
+import UserItem from "./components/users/UserItem"
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<NavBar />
+				<UserItem />
 			</div>
 		);
 	}
