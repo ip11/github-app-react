86e42da420c054cfeb304fb6b1badb80741dc4b4
ip11 <iprash011@gmail.com>
2020-02-22 14:43:32 +0530

    Making a state for text data

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+
+    state = {
+        text : ""
+    }
+
 	render() {
 		return (
 			<div>
 				<form className="form">
-					<input type="text" name="text" placeholder="Search Users .." />
+					<input type="text" name="text" placeholder="Search Users .." value={this.state.text}  />
 					<input type="submit" value="Search" className="btn btn-dark btn-block" />
 				</form>
 			</div>
