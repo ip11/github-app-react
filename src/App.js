976b7ece875251e3fa97ac21c74ac9106cea7e30
ip11 <iprash011@gmail.com>
2020-02-22 14:59:07 +0530

    Implemented Search Functionality

--- src/App.js
+++ src/App.js
@@ -20,8 +20,12 @@ class App extends React.Component {
 		this.setState({ users: res.data, loading: false });
 	}
 
-	searchUsers=(text)=>{
-		console.log(text);
+	searchUsers= async (text)=>{
+		this.setState({ loading: true });
+
+		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_SECRET}`);
+
+		this.setState({ users: res.data.items, loading: false });
 	}
 	render() {
 		return (
