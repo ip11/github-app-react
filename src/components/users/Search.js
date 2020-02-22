eabced0df0ed930ef1f99694f47f1298c5f84235
ip11 <iprash011@gmail.com>
2020-02-22 14:50:56 +0530

    Adding searchUsers MEthod in Search Component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-
-    state = {
-        text : ""
-    }
-    onChange=(e)=>{
-        this.setState({[e.target.name] : e.target.value})
-    }
-    onSubmit = (e)=>{
-        e.preventDefault();
-        console.log(this.state.text);
-     }
+	state = {
+		text: ''
+	};
+	onChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.props.searchUsers(this.state.text);
+		this.setState({ text: '' });
+	};
 
 	render() {
 		return (
 			<div>
 				<form className="form" onSubmit={this.onSubmit}>
-					<input type="text" name="text" placeholder="Search Users .." value={this.state.text} onChange={this.onChange} />
+					<input
+						type="text"
+						name="text"
+						placeholder="Search Users .."
+						value={this.state.text}
+						onChange={this.onChange}
+					/>
 					<input type="submit" value="Search" className="btn btn-dark btn-block" />
 				</form>
 			</div>
