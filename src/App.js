9f34905213078f499744bbfb0131e974ed8e4137
ip11 <iprash011@gmail.com>
2020-02-22 13:41:37 +0530

    Wrap In a Container

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 //Import Components
 import NavBar from './components/layouts/NavBar';
-import Users from "./components/users/Users";
+import Users from './components/users/Users';
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<NavBar />
-				<Users/>
+				<div className="container">
+					<Users />
+				</div>
 			</div>
 		);
 	}
