a7186380184a219090d44cf771dd268ec61f7714
ip11 <iprash011@gmail.com>
2020-03-26 12:49:22 +0530

    Implemented Link to stop href refresh

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import {Link} from 'react-router-dom';
 const NavBar = ({icon,title}) => {
 	return (
 		<nav className="navbar bg-primary">
@@ -9,11 +9,11 @@ const NavBar = ({icon,title}) => {
 			</h1>
 			<ul>
 				<li>
-					<a href="/"> Home</a>
+					<Link to="/"> Home</Link>
 
 				</li>
 				<li>
-					<a href="/about"> About </a>
+					<Link to="/about"> About </Link>
 				</li>
 			</ul>
 		</nav>
