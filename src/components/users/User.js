71a450e2a84e1ec79dd9756b75fd3e4bda164a0c
ip11 <iprash011@gmail.com>
2020-03-27 12:25:19 +0530

    Imported Spinner component and Back Link Button

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layouts/Spinner';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 export class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
-	}
+    }
+    
+    static propTypes = {
+        loading : PropTypes.bool,
+        user : PropTypes.object.isRequired,
+        getUser : PropTypes.func.isRequired
+    }
 	render() {
 		const {
 			name,
@@ -21,10 +30,16 @@ export class User extends Component {
         } = this.props.user;
         
         const {loading}= this.props;
+
+        if(loading){
+            return <Spinner />
+        }
 		return (
-			<div>
-				<h1> {name}</h1>
-			</div>
+			<Fragment>
+                <Link to='/' className='btn btn-light'>
+                    Back To Search
+                </Link>
+			</Fragment>
 		);
 	}
 }
