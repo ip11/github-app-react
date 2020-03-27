35596742171b70b3730d3caa21ef1de539306d13
ip11 <iprash011@gmail.com>
2020-03-27 12:41:56 +0530

    Bio and Visit Profile Button

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -48,8 +48,19 @@ export class User extends Component {
 				<div className="card grid-2">
 					<div className="all-center">
 						<img src={avatar_url} className="round-img" alt="Git Image" style={{ width: '150px' }} />
-                        <h1>{name}</h1>
-                        <p>Location : {location} </p>
+						<h1>{name}</h1>
+						<p>Location : {location} </p>
+					</div>
+					<div>
+						{bio && (
+							<Fragment>
+								<h3>Bio</h3>
+								<p>{bio}</p>
+							</Fragment>
+						)}
+						<a href={html_url} className="btn btn-dark my-1 ">
+							Visit GitHub Profile
+						</a>
 					</div>
 				</div>
 			</Fragment>
