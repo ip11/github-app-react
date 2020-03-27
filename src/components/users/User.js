ca526a6fee8b2cd65f5a5ff0c766b15eedcb9825
ip11 <iprash011@gmail.com>
2020-03-27 13:14:27 +0530

    Added getUsersRepos method in Users js component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -6,12 +6,14 @@ import { Link } from 'react-router-dom';
 export class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
+		this.props.getUserRepos(this.props.match.params.login);
 	}
 
 	static propTypes = {
 		loading: PropTypes.bool,
 		user: PropTypes.object.isRequired,
-		getUser: PropTypes.func.isRequired
+		getUser: PropTypes.func.isRequired,
+		getUserRepos: PropTypes.func.isRequired
 	};
 	render() {
 		const {
@@ -21,8 +23,8 @@ export class User extends Component {
 			bio,
 			blog,
 			login,
-            html_url,
-            company,
+			html_url,
+			company,
 			followers,
 			following,
 			public_repos,
@@ -62,39 +64,37 @@ export class User extends Component {
 						<a href={html_url} className="btn btn-dark my-1 ">
 							Visit GitHub Profile
 						</a>
-                        <ul>
-                            <li>
-                                {login && <Fragment>
-                                        <strong>Username : </strong> {login}
-                                    </Fragment>}
-                            </li>
-                            <li>
-                                {company && <Fragment>
-                                        <strong>Company : </strong> {company}
-                                    </Fragment>}
-                            </li>
-                            <li>
-                                {blog && <Fragment>
-                                        <strong>Website : </strong> {blog}
-                                    </Fragment>}
-                            </li>
-                        </ul>
+						<ul>
+							<li>
+								{login && (
+									<Fragment>
+										<strong>Username : </strong> {login}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{company && (
+									<Fragment>
+										<strong>Company : </strong> {company}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{blog && (
+									<Fragment>
+										<strong>Website : </strong> {blog}
+									</Fragment>
+								)}
+							</li>
+						</ul>
 					</div>
 				</div>
-                <div className='card text-center'>
-                    <div className='badge badge-primary'>
-                            Followers : {followers}
-                    </div>
-                    <div className='badge badge-success'>
-                            Following : {following}
-                    </div>
-                    <div className='badge badge-light'>
-                            Public Repositories : {public_repos}
-                    </div>
-                    <div className='badge badge-dark'>
-                            Public Gists : {public_gists}
-                    </div>
-                </div>
+				<div className="card text-center">
+					<div className="badge badge-primary">Followers : {followers}</div>
+					<div className="badge badge-success">Following : {following}</div>
+					<div className="badge badge-light">Public Repositories : {public_repos}</div>
+					<div className="badge badge-dark">Public Gists : {public_gists}</div>
+				</div>
 			</Fragment>
 		);
 	}
