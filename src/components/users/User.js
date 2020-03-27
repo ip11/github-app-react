f5da7e6e36deef72ea727939e0fb063ecff4b939
ip11 <iprash011@gmail.com>
2020-03-27 12:30:07 +0530

    Implemented Hirable Font

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,18 +1,18 @@
 import React, { Component, Fragment } from 'react';
 import Spinner from '../layouts/Spinner';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
-    }
-    
-    static propTypes = {
-        loading : PropTypes.bool,
-        user : PropTypes.object.isRequired,
-        getUser : PropTypes.func.isRequired
-    }
+	}
+
+	static propTypes = {
+		loading: PropTypes.bool,
+		user: PropTypes.object.isRequired,
+		getUser: PropTypes.func.isRequired
+	};
 	render() {
 		const {
 			name,
@@ -27,18 +27,24 @@ export class User extends Component {
 			public_repos,
 			public_gists,
 			hireable
-        } = this.props.user;
-        
-        const {loading}= this.props;
+		} = this.props.user;
 
-        if(loading){
-            return <Spinner />
-        }
+		const { loading } = this.props;
+
+		if (loading) {
+			return <Spinner />;
+		}
 		return (
 			<Fragment>
-                <Link to='/' className='btn btn-light'>
-                    Back To Search
-                </Link>
+				<Link to="/" className="btn btn-light">
+					Back To Search
+				</Link>
+				Hireable :{' '}
+				{hireable ? (
+					<i className="fas fa-check text-success" />
+				) : (
+					<i className="fas fa-times-circle text-danger" />
+				)}
 			</Fragment>
 		);
 	}
