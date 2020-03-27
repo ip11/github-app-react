4b78be19800ac123e4e763f5e980e6c998b9cb22
ip11 <iprash011@gmail.com>
2020-03-27 12:37:43 +0530

    User Page Grid component 1

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -45,6 +45,13 @@ export class User extends Component {
 				) : (
 					<i className="fas fa-times-circle text-danger" />
 				)}
+				<div className="card grid-2">
+					<div className="all-center">
+						<img src={avatar_url} className="round-img" alt="Git Image" style={{ width: '150px' }} />
+                        <h1>{name}</h1>
+                        <p>Location : {location} </p>
+					</div>
+				</div>
 			</Fragment>
 		);
 	}
